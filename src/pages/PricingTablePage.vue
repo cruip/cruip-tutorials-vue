9c04e57ee94edc3cec390d95195e7a6159cbc67e
pasqualevitiello <pasqualevitiello@gmail.com>
2023-05-17 14:33:46 +0200

    mc

--- src/pages/PricingTablePage.vue
+++ src/pages/PricingTablePage.vue
@@ -5,7 +5,7 @@ import Banner from '../components/Banner.vue'
 </script>
 
 <template>
-  <main class="min-h-screen flex flex-col justify-center bg-slate-50">
+  <main class="relative min-h-screen flex flex-col justify-center bg-slate-50 overflow-hidden">
     <div class="w-full max-w-6xl mx-auto px-4 md:px-6 py-24">
       <PricingTable />
     </div>
